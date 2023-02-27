a74ca48d51f7765dffa2732268dfb4353e9fb72b
fang360 <61006869+fang360@users.noreply.github.com>
2023-02-27 20:09:05 +0800

    Create code.py

---
+++ code.py
@@ -0,0 +1,5 @@
+# Introduction
+
+Customer segmentation is a vital strategy for businesses looking to allocate their resources efficiently. One of the most popular and effective methods of customer segmentation is RFM (Recency, Frequency, Monetary) analysis.
+RFM analysis is a behaviour-based approach that groups customers into segments based on their past purchase behaviour. It considers three factors: recency, frequency, and monetary value. Using these three factors, RFM analysis categorizes customers into segments and helps businesses identify the most profitable segments.
+  
